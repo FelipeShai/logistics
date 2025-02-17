49f2464ad7496f5db1640abfe19314917837c14d
FelipeShai <felpshai@gmail.com>
2025-02-17 02:11:09 -0300

    Fix unit test name

--- src/test/java/br/com/fiap/postech/logistics/usecases/courier/CreateCourierUseCaseTest.java
+++ src/test/java/br/com/fiap/postech/logistics/usecases/courier/CreateCourierUseCaseTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class CreateCourierUseCaseImplTest {
+class CreateCourierUseCaseTest {
 
     @Mock
     private CourierGateway courierGateway;
